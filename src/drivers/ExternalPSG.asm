300e18282fc1f76bbc64e0b87de193450e274d4c
Yeongman Seo <sharksym@hitel.net>
2020-06-06 18:17:48 +0900

    Drivers: Added ExternalPSG support for T-Wave
    Sec-PSG EMU supported
    
    Signed-off-by: Yeongman Seo <sharksym@hitel.net>

--- src/drivers/ExternalPSG.asm
+++ src/drivers/ExternalPSG.asm
@@ -6,6 +6,7 @@ ExternalPSG_MFRSD_ID_ADDRESS: equ 04010H
 ExternalPSG_TYPE_READWRITE: equ 0
 ExternalPSG_TYPE_WRITEONLY: equ 1
 ExternalPSG_TYPE_MFRSD: equ 2
+ExternalPSG_TYPE_TWAVE: equ 3
 
 ExternalPSG: MACRO
 	super: PSG ExternalPSG_BASE_PORT, ExternalPSG_nameReadWrite
@@ -25,8 +26,14 @@ ExternalPSG_Construct:
 	call Driver_SetName
 	jr PSG_Reset
 
-ExternalPSG_Destruct: equ PSG_Destruct
-;	jp PSG_Destruct
+;ExternalPSG_Destruct: equ PSG_Destruct
+; ix = this
+ExternalPSG_Destruct:
+	call PSG_Destruct
+	ld a,(ix + ExternalPSG.type)
+	cp ExternalPSG_TYPE_TWAVE
+	ret nz
+	jp TwavePSG_Destruct
 
 ; ix = this
 ; hl <- name
@@ -38,6 +45,9 @@ ExternalPSG_GetName:
 	ld hl,ExternalPSG_nameWriteOnly
 	ret z
 	ld hl,ExternalPSG_nameMFRSD
+	cp ExternalPSG_TYPE_MFRSD
+	ret z
+	ld hl,ExternalPSG_nameTWAVE
 	ret
 
 ; ix = this
@@ -51,6 +61,9 @@ ExternalPSG_Detect:
 	call Memory_SearchSlots
 	ld a,ExternalPSG_TYPE_MFRSD
 	ret c
+	call TwavePSG_Detect
+	ld a,ExternalPSG_TYPE_TWAVE
+	ret c
 	ld a,ExternalPSG_TYPE_WRITEONLY
 	scf
 	ret
@@ -62,6 +75,39 @@ ExternalPSG_MatchMFRSDID:
 	ld hl,ExternalPSG_MFRSD_ID_ADDRESS
 	ld bc,5
 	jp Memory_MatchSlotString
+
+; a = slot id
+; f <- c: found
+TwavePSG_Detect:
+	in a,(040h)
+	cpl
+	ld b,a
+	ld a,0d8h
+	out (040h),a
+	in a,(040h)
+	cp 027h
+	scf
+	jr nz,TwavePSG_Detect_end
+	ld a,080h
+	out (045h),a
+	ccf
+TwavePSG_Detect_end:
+	ccf
+	ld a,b
+	out (040h),a
+	ret
+
+TwavePSG_Destruct:
+	in a,(040h)
+	cpl
+	ld b,a
+	ld a,0d8h
+	out (040h),a
+	ld a,0
+	out (045h),a
+	ld a,b
+	out (040h),a
+	ret
 
 ;
 	SECTION RAM
@@ -80,6 +126,9 @@ ExternalPSG_nameWriteOnly:
 
 ExternalPSG_nameMFRSD:
 	db "MegaFlashROM SD PSG",0
+
+ExternalPSG_nameTWAVE:
+	db "T-Wave PSG",0
 
 ExternalPSG_mfrsdId:
 	db "MFRSD"
